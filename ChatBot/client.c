9741826e26c0b6ddea4acfea49b56d0dedfc1d36
poonamshettar <poonammshettar123@gmail.com>
2024-04-25 18:52:03 +0530

    Chatbot using TCP

---
+++ ChatBot/client.c
@@ -0,0 +1,39 @@
+
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+int main(int argc, char **argv)
+{
+int n,sockfd,port;
+char buffer[256];
+struct sockaddr_in server_addr;
+port=atoi(argv[1]);
+if((sockfd=socket(AF_INET,SOCK_STREAM,0))==-1)
+{
+        fprintf(stderr,"cant create a socket\n");
+        exit(1);
+}
+
+server_addr.sin_family=AF_INET;
+server_addr.sin_port=htons(port);
+server_addr.sin_addr.s_addr=inet_addr("127.0.0.1");
+
+ if(connect(sockfd,(struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
+    {
+        fprintf(stderr,"Can't connect\n");
+        exit(1);
+    }
+    printf("Enter message\n");
+    scanf("%s",buffer);
+    write(sockfd,buffer,sizeof(buffer));
+    n=read(sockfd,buffer,sizeof(buffer));
+    printf("Message recieved from server: %s\n",buffer);
+    printf("%s\n",buffer);
+    close(sockfd);
+}
+
---
+++ ChatBot/server.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+int main(int argc,char **argv)
+{
+int socketfd,clientfd,port;
+int n,yes=1;
+char buffer[256],msg[256];
+struct sockaddr_in server_addr,client_addr;
+port=atoi(argv[1]);
+
+if((socketfd=socket(AF_INET,SOCK_STREAM,0))==-1)
+{
+fprintf(stderr,"Socket not created\n");
+exit(1);
+}
+
+server_addr.sin_family=AF_INET;
+server_addr.sin_port=htons(port);
+server_addr.sin_addr.s_addr=inet_addr("127.0.0.1");
+
+if(bind(socketfd,(struct sockaddr *)&server_addr,sizeof(server_addr))==-1)
+{
+fprintf(stderr,"cant bind\n");
+exit(1);
+}
+
+listen(socketfd,5);
+
+while(1)
+{
+int len=sizeof(client_addr);
+if((clientfd=accept(socketfd,(struct sockaddr *)&client_addr,&len))==-1)
+{
+fprintf(stderr,"cant accept client\n");
+exit(1);
+}
+n=read(clientfd,buffer,sizeof(buffer));
+printf("message sent by client: %s\n",buffer);
+printf("Enter message to client: \n");
+scanf("%s",buffer);
+write(clientfd,buffer,n);
+close(clientfd);
+}
+close(socketfd);
+return 0;
+}
